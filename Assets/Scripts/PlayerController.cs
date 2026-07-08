5176d7ee8b76d2249d91292b5265f3390ebdcd65
Nejat Alper Satiroglu <n.satiroglu265@mybvc.ca>
2026-07-08 12:43:40 -0600

    Move Input actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,12 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Rigidbody _rb;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 }
